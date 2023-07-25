9bf65cf7d2796a773ff5a2483cbad3504d9c36d1
Wender Barbosa <wenblack012@gmail.com>
2023-07-25 11:40:45 -0300

    ➕  Created tasksCard and tags components ➕

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,7 @@
 import editIcon from "@/assets/pencil.svg"
 import Image from "next/image"
 import {IoFilter, IoSearch} from 'react-icons/io5'
-import { Button } from "@/components/ui/button"
+import { Task } from "./Task"
 
 export function Table (){
     return(
@@ -11,7 +11,7 @@ export function Table (){
                 <img className="rounded-full" src="https://github.com/wenblack.png" width={64} height={64} alt="avatar" />
             </section>
         
-            <form className="flex mt-8 items-center gap-4 text-2xl" >
+            <form className="flex mt-8 items-center gap-4 text-3xl" >
                 <button className="bg-[#7c3aed] hidden gap-4 items-center justify-center md:flex lg:flex text-white  hover:opacity-70 rounded-lg py-5 px-12">
                     <IoFilter  ></IoFilter>
                     Filtrar
@@ -26,6 +26,64 @@ export function Table (){
                     <button className="lg:hidden md:hidden" type="button"><IoFilter/></button>
                 </div>
             </form>
+
+
+            <section className="kanban grid mt-32 grid-cols-3 space-x-8">
+            <div className="todo grid ">
+              <h2 className="font-bold text-3xl mb-8">A fazer</h2>
+              <div className="cards">
+                <Task 
+                    title="#boraCodar um Kanban 🧑🏾‍💻"
+                    content="Novo desafio do #boraCodar da Rocketseat, onde é proposto
+                    construir um quadro de Kanban."
+                    tagOne="adas"
+                    tagTwo="asdas"
+                />
+                <div className="card">
+                  <h3>Manter a ofensiva 🔥</h3>
+                  <p>
+                    Manter minha atividade na plataforma da Rocketseat para não
+                    perder a ofensiva
+                  </p>
+                  <div className="tags">
+                    <span>rocketseat</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="doing   ">
+              <h2 className="font-bold text-3xl mb-8">Fazendo</h2>
+              <div className="cards">
+                <div className="card">
+                  <h3>Conferir o novo desafio 🚀</h3>
+                  <p>
+                    Conferir o novo projeto do #boraCodar para fazê-lo da melhor
+                    maneira possível
+                  </p>
+                  <div className="tags">
+                    <span>rocketseat</span>
+                    <span>desafio</span>
+                  </div>
+                </div>
+              </div>
+        </div>
+            <div className="done ">
+              <h2 className="font-bold text-3xl mb-8">Feito</h2>
+              <div className="cards">
+                <div className="card">
+                  <h3>#boraCodar uma página de login 🧑‍💻</h3>
+                  <p>
+                    Novo desafio do #boraCodar da Rocketseat, onde é proposto
+                    construir uma página de login.
+                  </p>
+                  <div className="tags">
+                    <span>rocketseat</span>
+                    <span>desafio</span>
+                  </div>
+                </div>
+              </div>
+        </div>
+          </section>
         </>
     )
 }
---
+++ src/components/Task.tsx
@@ -0,0 +1,40 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Tag } from "./Tag"
+
+interface cardProps{
+    title:string
+    content:string,
+    hasTags?:boolean
+    tagOne?:string
+    tagTwo?:string
+}
+
+export function Task ({content, title, tagOne, tagTwo}:cardProps){
+    return(
+        <Card className="w-full max-w-[300px] shadow-xl">
+            <CardHeader>
+                <CardTitle className="text-xl">{title}</CardTitle>
+            </CardHeader>
+  
+            <CardContent>
+                <p className="text-gray-800 text-lg">
+                    {content}
+                </p>
+            </CardContent>
+            
+            <CardFooter className="flex justify-around">
+                {tagOne ===undefined ?<></>:<Tag value={tagOne}/>}
+                {tagTwo ===undefined ?<></>:<Tag value={tagTwo}/>}
+              
+            </CardFooter>   
+        
+        </Card>
+
+    )
+}
